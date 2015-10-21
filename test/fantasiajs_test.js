c5cc463c0740076b6fdbe337bb9e9f807bf92d17
Emil Møller <emilrmoeller@gmail.com>
2015-10-21 12:25:26 +0200

    Added dist

--- test/fantasiajs_test.js
+++ test/fantasiajs_test.js
@@ -1,12 +1,12 @@
-(function ($) {
-  module(':fantasiajs selector', {
-    setup: function () {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
+// (function ($) {
+//   module(':fantasiajs selector', {
+//     setup: function () {
+//       this.elems = $('#qunit-fixture').children();
+//     }
+//   });
 
-  test('is fantasiajs', function () {
-    expect(1);
-    deepEqual(this.elems.filter(':fantasiajs').get(), this.elems.last().get());
-  });
-}(jQuery));
+//   test('is fantasiajs', function () {
+//     expect(1);
+//     deepEqual(this.elems.filter(':fantasiajs').get(), this.elems.last().get());
+//   });
+// }(jQuery));
